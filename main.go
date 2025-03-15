cd9531c923c00f748dffd38ca01079960951aac6
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-14 20:54:26 -0400

    added imports and implemented cd

--- main.go
+++ main.go
@@ -1,51 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("> ")
-		// read keyboard input
+		// Read the keyboad input.
 		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, err)
 		}
-		// handle execution
+
+		// Handle the execution of the input.
 		if err = execute_input(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 }
 
+// ErrNoPath is returned when 'cd' was called without a second argument.
+var ErrNoPath = errors.New("path required")
+
 func execute_input(input string) error {
-	//  remove newline char
+	// Remove the newline character.
 	input = strings.TrimSuffix(input, "\n")
 
-	// split on spaces to separate command and arguments
+	// Split the input separate the command and the arguments.
 	args := strings.Split(input, " ")
 
-	// check for built-in commands
+	// Check for built-in commands.
 	switch args[0] {
 	case "cd":
-		// cd to home with no path not implemented yet
+		// 'cd' to home with empty path not yet supported.
 		if len(args) < 2 {
-			return errors.New("cd: no such file or directory")
+			return ErrNoPath
 		}
-		// change directory
+		// Change the directory and return the error.
 		return os.Chdir(args[1])
 	case "exit":
-		// exit the shell
 		os.Exit(0)
 	}
 
-	// pass arguments
+	// Prepare the command to execute.
 	cmd := exec.Command(args[0], args[1:]...)
 
-	// prepare command
-	cmd := exec.Command(input)
-
-	// set output device
-	cmd.Stdout = os.Stdout
+	// Set the correct output device.
 	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
 
-	// execute and return error
+	// Execute the command and return the error.
 	return cmd.Run()
 }
-
